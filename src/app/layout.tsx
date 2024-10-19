becd1ade02a9ac9d061ec1b8d89abcaf82a7baf4
codingpylypiak <pylypiaklvivwork1@gmail.com>
2024-10-19 18:07:23 +0300

    over

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable}  bg-white `}
+        className={`${inter.variable}  bg-white  overflow-x-hidden`}
         >
         <Header/>
         <section>
